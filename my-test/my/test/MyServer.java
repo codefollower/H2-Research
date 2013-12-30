b79c23554e8d4a3a7162f26864a7a3a63a7befc3
Honghua Zhu <zhh200910@gmail.com>
2013-12-30 21:46:21 +0800

    更改测试输出目录

--- my-test/my/test/MyServer.java
+++ my-test/my/test/MyServer.java
@@ -8,8 +8,8 @@ public class MyServer {
 		// System.setProperty("DATABASE_TO_UPPER", "false");
 		System.setProperty("h2.lobInDatabase", "false");
 		System.setProperty("h2.lobClientMaxSizeMemory", "1024");
-		System.setProperty("java.io.tmpdir", "E:\\H2\\tmp");
-		System.setProperty("h2.baseDir", "E:\\H2\\baseDir");
+		System.setProperty("java.io.tmpdir", "target/mytest/tmp");
+		System.setProperty("h2.baseDir", "target/mytest/baseDir");
 		//System.setProperty("h2.check2", "true");
 		ArrayList<String> list = new ArrayList<String>();
 		// list.add("-tcp");
@@ -25,35 +25,10 @@ public class MyServer {
 		// list.add("mydb");
 		// list.add("mydatabase");
 
-		list.add("-pg");
+		//list.add("-pg");
 		list.add("-tcp");
 		//list.add("-web");
 		org.h2.tools.Server.main(list.toArray(new String[list.size()]));
 	}
 }
 
-/*
- * 
- * public static void main(String[] args) throws SQLException {
- * ArrayList<String> list = new ArrayList<String>(); //list.add("-?");
- * list.add("-tcp"); //list.add("-web-showUsageAndThrowUnsupportedOption");
- * 
- * //list.add("-throwUnsupportedOption");
- * 
- * //list.add("-tcpShutdown");list.add("tcp:localhost");
- * 
- * org.h2.tools.Server.main(list.toArray(new String[list.size()]));
- * 
- * } public static void main2(String[] args) throws SQLException {
- * ArrayList<String> list = new ArrayList<String>(); //list.add("-?");
- * list.add("-tcpShutdown"); list.add("tcp://192.168.1.106:9092");
- * //list.add("-web-showUsageAndThrowUnsupportedOption");
- * 
- * //list.add("-throwUnsupportedOption");
- * 
- * //list.add("-tcpShutdown");list.add("tcp:localhost");
- * 
- * org.h2.tools.Server.main(list.toArray(new String[list.size()]));
- * 
- * }
- */
